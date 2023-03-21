476587ea40fd0ab59751a06115f097450686e17c
SagiKPJH1021 <Juhyung1021@gmail.com>
2023-03-22 08:10:06 +0900

    docker bat upgrade

--- cuda-vscode run juhyung1021_Docker.bat
+++ cuda-vscode run juhyung1021_Docker.bat
@@ -17,14 +17,15 @@ REM 실행중인 Docker 컨테이너 검색
 set EXISTING_CONTAINER=
 for /f "delims=" %%i in ('docker ps -aq -f "name=%CONTAINER_NAME%"') do set EXISTING_CONTAINER=%%i
 
+set /p PORT="Enter port number: "
+
 REM Docker 컨테이너 실행
 if not "%EXISTING_CONTAINER%"=="" (
   echo Container already exists. Running existing container %CONTAINER_NAME%...
   docker start %EXISTING_CONTAINER%
 ) else (
-  set /p PORT="Enter port number: "
   echo Creating new container %CONTAINER_NAME%...
-  docker run -it --gpus all --name %CONTAINER_NAME% -p 9000:8080 %IMAGE_NAME%:lastest
+  docker run -it --gpus all --name %CONTAINER_NAME% -p %PORT%:8080 %IMAGE_NAME%:lastest
 )
 
 pause
